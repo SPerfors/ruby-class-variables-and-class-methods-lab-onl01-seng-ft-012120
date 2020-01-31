bccf6a29d624bedc2a948dd96f31c16499ae616d
Steven Perfors <stevecanbuild@gmail.com>
2020-01-31 20:52:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,10 @@ class Song
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
-    @genres = genre 
+    @genre = genre 
     @@count += 1 
+    @@artist << artist 
+    @@genres << genre 
   end
   
   def self.count
