20ed54524d9675d9fd30ff68d810aa767845b1e1
Steven Perfors <stevecanbuild@gmail.com>
2020-01-31 21:15:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     genre_count = {} 
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1 
+        genre_count[genre] += 1 
       else 
         genre_count[genre] = 1 
       end 
@@ -42,7 +42,12 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
   end
   
 end
